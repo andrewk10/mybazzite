9cb0f51e30298b28b2220bd08429e808bcf475ed
Andrew Kenneally <19553807+andrewk10@users.noreply.github.com>
2024-11-06 19:53:12 +0000

    Moving the directory creation into the build.sh

--- build.sh
+++ build.sh
@@ -12,6 +12,8 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+# To install Mullvad we need the appropriate opt directory in place.
+mkdir -p /opt/Mullvad\ VPN/resources/mullvad-setup
 rpm-ostree install /tmp/MullvadVPN-2024.7_x86_64.rpm
 
 #### Example for enabling a System Unit File
